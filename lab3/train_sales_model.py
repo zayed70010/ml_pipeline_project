ebd4ec1eea949cf10c90c3a35696b404dc34756f
zayed <zayed>
2025-04-16 01:49:53 +0500

    Initial commit for ML pipeline project

--- lab3/train_sales_model.py
+++ lab3/train_sales_model.py
@@ -1,36 +1,42 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import mlflow
 import mlflow.sklearn
 
-# تحميل البيانات
-df = pd.read_csv("data/vehicles_dataset_prepared.csv")
+df = pd.read_csv('./data/sales.csv')
 
-# تجهيز البيانات
-X = df.drop("Price", axis=1)
-y = df["Price"]
+encoder = LabelEncoder()
+df.Street = encoder.fit_transform(df.Street)
+df.SaleCondition = encoder.fit_transform(df.SaleCondition)
+df['TotalArea'] = df['GrLivArea'] + df['GarageArea']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x = df[['GrLivArea', 'GarageArea', 'OverallQual', 'Street', 'SaleCondition', 'TotalArea']]
+y = df['SalePrice']
 
-# تفعيل mlflow لتتبع التجربة
-mlflow.set_experiment("linear_regression_car_prices")
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=123)
 
+model = LinearRegression()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+mlflow.set_experiment("linear model sales")
 with mlflow.start_run():
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-
-    predictions = model.predict(X_test)
-
-    r2 = r2_score(y_test, predictions)
-    mse = mean_squared_error(y_test, predictions)
-
-    # تسجيل القيم في mlflow
-    mlflow.log_metric("r2_score", r2)
+    mlflow.log_param("model_type", "LinearRegression")
+    mlflow.log_metric("mae", mae)
     mlflow.log_metric("mse", mse)
-
-    # تسجيل النموذج
+    mlflow.log_metric("r2", r2)
     mlflow.sklearn.log_model(model, "model")
+    best_model_path = mlflow.get_artifact_uri("model")
 
-    print("Model trained and logged with MLflow.")
+with open("best_model.txt", "w") as f:
+    f.write(best_model_path)
+
+print("✅ Training done and model path saved.")
