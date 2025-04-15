43d90ae8fa8cdcb16020f920629ad4f70a2c81df
zayed <zayed>
2025-04-16 01:44:04 +0500

    تحديث السكربتات والملفات النهائية للمشروع

--- lab3/prepare_sales_data.py
+++ lab3/prepare_sales_data.py
@@ -1,15 +1,7 @@
-import shutil
-import os
+import pandas as pd
 
-# تعريف المسارات
-source_path = "../data/vehicles_dataset_prepared.csv"
-destination_path = "vehicles_dataset_prepared.csv"
+df = pd.read_csv('./data/sales.csv')
 
-# نسخ الملف من مجلد data إلى lab3
-try:
-    shutil.copy(source_path, destination_path)
-    print(f"✅ File copied to: {destination_path}")
-except FileNotFoundError:
-    print("❌ Source file not found. Make sure it exists at: data/vehicles_dataset_prepared.csv")
-except Exception as e:
-    print(f"❌ Error during file copy: {e}")
+df.to_csv('./data/sales.csv', index=False)
+
+print("✅ Download stage done.")
